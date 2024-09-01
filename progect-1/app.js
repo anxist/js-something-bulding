e01ec0fc0bd94f27637b438f2e27549f411f27e5
Diana Narizhnyaya <82086491+anxist@users.noreply.github.com>
2024-09-01 17:53:53 +0300

    Добавление описания первой работы в README.md

--- progect-1/app.js
+++ progect-1/app.js
@@ -5,10 +5,54 @@ for (const slide of slides) {
         clearActiveClasses()
         slide.classList.add('active')
     })
+
+    // Добавляем поддержку свайпов для мобильных устройств
+    slide.addEventListener('touchstart', handleTouchStart, false)
+    slide.addEventListener('touchmove', handleTouchMove, false)
 }
 
 function clearActiveClasses() {
     slides.forEach((slide) => {
         slide.classList.remove('active')
     })
+}
+
+let x1 = null
+
+function handleTouchStart(event) {
+    const firstTouch = event.touches[0]
+    x1 = firstTouch.clientX
+}
+
+function handleTouchMove(event) {
+    if (!x1) {
+        return false
+    }
+
+    let x2 = event.touches[0].clientX
+    let xDiff = x2 - x1
+
+    if (xDiff > 0) {
+        // Свайп вправо
+        moveSlide('prev')
+    } else {
+        // Свайп влево
+        moveSlide('next')
+    }
+
+    x1 = null
+}
+
+function moveSlide(direction) {
+    const activeSlide = document.querySelector('.slide.active')
+    let newIndex = Array.from(slides).indexOf(activeSlide)
+
+    if (direction === 'next') {
+        newIndex = (newIndex + 1) % slides.length
+    } else if (direction === 'prev') {
+        newIndex = (newIndex - 1 + slides.length) % slides.length
+    }
+
+    clearActiveClasses()
+    slides[newIndex].classList.add('active')
 }
